6127d2ac38e9ab913f0c13fc34ba4c76936d424b
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-12-25 14:39:45 -0500

    improve the profile form button

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { updateGuest } from "@/app/_lib/actions";
+import { useFormStatus } from "react-dom";
 
 function UpdateProfileForm({ children, guest }) {
 	const { fullName, email, nationality, nationalID, countryFlag } = guest;
@@ -54,11 +55,24 @@ function UpdateProfileForm({ children, guest }) {
 			</div>
 
 			<div className="flex justify-end items-center gap-6">
-				<button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-					Update profile
-				</button>
+				<Button />
 			</div>
 		</form>
+	);
+}
+
+function Button() {
+	// this hook from React-DOM have to be in a component that is in the form
+	// it can't be the component that contain the form
+	const { pending } = useFormStatus();
+
+	return (
+		<button
+			className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+			disabled={pending}
+		>
+			{pending ? "Updating..." : "Update profile"}
+		</button>
 	);
 }
 
